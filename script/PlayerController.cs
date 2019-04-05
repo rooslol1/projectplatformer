7853f2c7890be17bff46470aebb2c780351b46c0
rooslol1 <43379522+rooslol1@users.noreply.github.com>
2019-04-06 00:45:52 +0200

    Add files via upload

---
+++ script/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float playerSpeed;
+    public float jumpSpeed;
+
+    private bool isJumping;
+    private float move;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {       //movement -1 of 1 
+        move = Input.GetAxis("Horizontal");
+        //
+        rb.velocity = new Vector2(move * playerSpeed, rb.velocity.y);
+
+        if (move > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }else if (move < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+
+
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jumpSpeed));
+            isJumping = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+       if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag== "spike")
+        {
+            Scene scene;
+            scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+         
+        }
+    }
+}
---
+++ script/camerafollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerafollow : MonoBehaviour
+{ //volgt de player
+    private Transform playerTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        
+    }
+
+    
+    void LateUpdate()
+    {   
+        // je slaat je camera positie op in variable temp
+        Vector3 temp = transform.position;
+
+        //je zetten de camera positie x gelijk aan de players x
+        temp.x = playerTransform.position.x;
+
+        //je zet de camera terug op de newe positie
+        transform.position = temp;
+    }
+}
